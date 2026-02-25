c9daf33c1e5ec65e1ac60a5cb4289baa7a1942e1
Pratik <pratikgajjar1827@gmail.com>
2026-02-25 21:12:54 +0530

    backedn

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,12 @@
+export type AmplifyDependentResourcesAttributes = {
+  "function": {
+    "medicsaltour2cbbb858": {
+      "Arn": "string",
+      "CloudWatchEventRule": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  }
+}
